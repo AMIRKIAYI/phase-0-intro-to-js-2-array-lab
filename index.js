a68ccfdc77d5c3fa7e785d204cea4ddc15bbc065
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-27 12:17:04 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("tos");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
